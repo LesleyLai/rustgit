0ee4fe2e9c60f16f38869f8f17ce9a0affb11ae4
Lesley Lai <lesley@lesleylai.info>
2024-04-11 03:02:06 +0800

    Implements --stdin for hash-object

--- src/commands/hash_object.rs
+++ src/commands/hash_object.rs
@@ -1,7 +1,17 @@
+use anyhow::Context;
 use clap::Args;
 use rustgit_plumbing::hash::Sha1Hash;
 use rustgit_plumbing::object::{write_object, Object, ObjectType};
 use std::fs;
+use std::io::Read;
+
+#[derive(Args, Debug)]
+#[group(required = true, multiple = false)]
+pub struct HashObjectArgsGroup {
+    #[clap(long)]
+    stdin: bool,
+    filename: Option<String>,
+}
 
 #[derive(Args, Debug)]
 pub struct HashObjectArgs {
@@ -9,11 +19,21 @@ pub struct HashObjectArgs {
     #[clap(short = 'w')]
     perform_write: bool,
 
-    filename: String,
+    #[clap(flatten)]
+    group: HashObjectArgsGroup,
 }
 
 pub fn hash_object(args: HashObjectArgs) -> anyhow::Result<()> {
-    let body = fs::read_to_string(args.filename)?;
+    let body = if args.group.stdin {
+        let mut input = String::new();
+        std::io::stdin()
+            .read_to_string(&mut input)
+            .context("Failed to read from stdin")?;
+        input
+    } else {
+        fs::read_to_string(args.group.filename.unwrap())?
+    };
+
     let blob = Object::new(ObjectType::Blob, body.as_bytes());
     let object_hash = Sha1Hash::from_data(&blob.data);
     println!("{}", object_hash.to_hex_string());
