199a2a07cf5a1cdcd595827eb64d9c24cea268f9
Lesley Lai <lesley@lesleylai.info>
2024-04-17 18:24:47 +0800

    use insta test for write-tree

--- tests/integration/write_tree.rs
+++ tests/integration/write_tree.rs
@@ -1,6 +1,6 @@
 use crate::common::{git, populate_folder, rustgit, test_path};
 use assert_cmd::prelude::*;
-use predicates::prelude::*;
+use std::str::from_utf8;
 
 #[test]
 fn write_tree() -> anyhow::Result<()> {
@@ -14,15 +14,12 @@ fn write_tree() -> anyhow::Result<()> {
 
     let tree_hash = rustgit::new_command(&working_dir).write_tree()?;
 
-    let expected = "040000 tree 91e1483644d087af54a6e8aac15a08c482bb9fb1\tdir1
-040000 tree cf8e933fedbe540f9881ba4dc34b034785834227\tdir2
-100644 blob b6fc4c620b67d95f953a5c1c1230aaab5db5a1b0\tfile1.txt";
-
-    git::new_command(&working_dir)
+    let command = git::new_command(&working_dir)
         .args(["ls-tree", &tree_hash])
         .assert()
-        .success()
-        .stdout(predicate::str::starts_with(expected));
+        .success();
+    let output = from_utf8(&command.get_output().stdout)?;
+    insta::assert_snapshot!(output);
 
     Ok(())
 }
